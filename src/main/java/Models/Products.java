5f3186c9cfe7338ab724bb904a69c52244611375
ranchayasambath <ranchayasambath@gmail.com>
2022-04-04 12:41:24 -0500

    class notes

---
+++ src/main/java/Models/Products.java
@@ -0,0 +1,9 @@
+package Models;
+
+import java.util.List;
+
+public interface Products {
+    List<Product> all(); // get all products and return as a list (can poly into an arraylist)
+
+    void insert(Product productToInsert); // insert into list
+}
